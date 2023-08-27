5238652834dc6e1a26414c1e5b4f3ffd65b65745
matheusDev <matheus.info29@gmail.com>
2023-08-27 12:08:34 -0300

    Exercício 01 - Que danado é JavaScript

--- Aula 1 - Que danado é JavaScrip/scrips.js
+++ Aula 1 - Que danado é JavaScrip/scrips.js
@@ -1,1 +1,31 @@
-alert('Matheus lindão <3')
+
+/*
+//Number
+var n1 = 10;
+console.log(typeof(n1));
+
+//Boolean
+ var Matheus_e_lindo = true;
+ console.log(Matheus_e_lindo);
+ console.log(typeof(Matheus_e_lindo));
+
+ //String
+ var matheus = 'Matheus é lindão';
+ console.log(matheus);
+ console.log(typeof(matheus));
+
+ 
+*/
+
+
+
+// var escopo = 'Global';
+// console.log(escopo);
+
+// function escopoLocal(params) {
+//     let escopoLocal ='locaaaaaaaaaaaaaaal';
+//     console.log(escopoLocal);
+// }
+
+// escopoLocal();
+
