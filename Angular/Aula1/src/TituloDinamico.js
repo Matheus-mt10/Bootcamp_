b13bfff577296600633f9cbb4210ec0fc9e61d7d
matheusDev <matheus.info29@gmail.com>
2023-10-10 22:55:16 -0300

    update

---
+++ Angular/Aula1/src/TituloDinamico.js
@@ -0,0 +1,15 @@
+class   TituloDinamico extends HTMLElement{
+    constructor(){
+        super();
+
+        const shadow   = this.attachShadow({mode: "open"});
+
+        // base do componente
+        const componentRoot = 
+        // estilizar o component 
+
+        // enviar a shadow
+    }
+}
+
+customElements.define('titulo-dinamico', TituloDinamico);
